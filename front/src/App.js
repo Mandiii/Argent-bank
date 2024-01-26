f927d18eccea2383b36c68a8c8cf3a5f82e74f9b
Mandi <mandi.gansauge@gmail.com>
2024-01-26 12:42:13 +0100

    Install React router & redux start

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,17 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import User from "./pages/User";
+import {Route, Routes } from 'react-router-dom'
 import "./styles/main.css"
 function App() {
   return (
     <>
       <Header/>
-      <Login/>
-      <User/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/user" element={<User/>}/>
+      </Routes>
       <Footer/>
     </>
   );
--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,22 +1,24 @@
 import logo from '../img/argentBankLogo.png';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
   return (
     <nav class="main-nav">
-        <a class="main-nav-logo" href="#">
+      <Link className='main-nav-logo' to="/">
         <img
             class="main-nav-logo-image"
             src={logo}
             alt="Argent Bank Logo"
         />
         <h1 class="sr-only">Argent Bank</h1>
-        </a>
+        
+      </Link>
         <div>
-        <a class="main-nav-item" href="#">
+          <Link className='main-nav-item' to="/login">
             <i class="fa fa-user-circle"></i>
             Sign In
-        </a>
+            </Link>
         </div>
     </nav>
   );
