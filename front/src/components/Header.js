3b7c742c71ba8c93eefad80255a6fef20675571b
Mandi <mandi.gansauge@gmail.com>
2024-03-17 10:39:51 +0100

    Small changes to header

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,7 +1,16 @@
 import logo from '../img/argentBankLogo.png';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from "../state/user/userSlice";
 
 function Header() {
+  const dispatch = useDispatch()
+  const token = useSelector((state) => state.userInfo.value.token)
+  function signout() {
+    dispatch(logout())
+  }
+
+
   return (
     <nav className="main-nav">
       <Link className='main-nav-logo' to="/">
@@ -14,10 +23,7 @@ function Header() {
         
       </Link>
         <div>
-          <Link className='main-nav-item' to="/login">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-            </Link>
+          {token? <Link onClick={() =>signout()} className='main-nav-item' to="/">Sign out</Link> : <Link className='main-nav-item' to="/login"><i className="fa fa-user-circle"></i>Sign In</Link>}
         </div>
     </nav>
   );
